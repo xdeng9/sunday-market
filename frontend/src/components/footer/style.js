63ad9e002914b159992ce76ac4e6db597ef29495
Joseph Deng <xdeng9@gmail.com>
2020-05-12 11:53:52 -0700

    Add style to home page

---
+++ frontend/src/components/footer/style.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const FooterBox = styled.div`
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    display: flex;
+    margin-bottom: 0px;
+    margin-top: 400px;
+    justify-content: center;
+    width: 100%;
+    height: 200px;           
+    background-color:rgb(241, 241, 241);
+    border-top: 1px solid #ccc;
+    color: gray;
+`
+export const FooterNav =styled.div`
+    display: block;
+    justify-content: center;
+    margin-top: 50px;
+`
+export const Icon = styled.a`
+    font-size: 28px;
+    margin: 0 70px;
+`
+
+export const CopyRight = styled.div`
+    display:block;
+    font-size: 18px;
+    margin-top: 30px; 
+`
